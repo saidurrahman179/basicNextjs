9f3b7cb0484730b63a57c818ed7505f839e52818
saidur rahman <saidurrahman179@gmail.com>
2024-10-10 10:04:33 +0600

    again change home page style

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ title:"Home page|saidur"
 
 export default function Home() {
   return (
-    <div className="m-10 flex items-center justify-center">
+    <div className="m-10 flex items-center justify-center bg-lime-950 text-white">
       <h1>This is Main Home  page</h1>
     </div>
   );
