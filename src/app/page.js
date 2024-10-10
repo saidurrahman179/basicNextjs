0b07e05f0f0f985ec424cc9b631b691071592271
saidur rahman <saidurrahman179@gmail.com>
2024-10-10 10:02:47 +0600

    center

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ title:"Home page|saidur"
 
 export default function Home() {
   return (
-    <div>
+    <div className="m-10 flex items-center justify-center">
       <h1>This is Main Home  page</h1>
     </div>
   );
